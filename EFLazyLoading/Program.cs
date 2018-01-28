d17f7df562bed1f616269a2c4295f248900e4104
Volkan <gencvolkan@gmail.com>
2018-01-28 07:28:32 -0800

    jj

--- EFLazyLoading/Program.cs
+++ EFLazyLoading/Program.cs
@@ -10,11 +10,12 @@ namespace EFLazyLoading
         static void Main(string[] args)
         {
             using (var context = new BookContext())
+
             {
                 var author = context.Authors.Select(x => new { b = x.Books.AsQueryable().Select(c => c.Title).ToList() });
 
-                foreach (var b in author.b)
-                    Console.WriteLine(b.Title);
+                //foreach (var b in author.b)
+                //  Console.WriteLine(b.Title);
             }
 
             Console.ReadLine();
